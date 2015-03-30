42eb3d99a8653a34798dd6220dc02bf14d3686c8
Drew Zoellner <andrewzoellner@hotmail.com>
2015-03-30 16:38:46 -0500

    working store background and buttons

--- RPGWARRIOR/StoreScene.swift
+++ RPGWARRIOR/StoreScene.swift
@@ -11,8 +11,8 @@ import SpriteKit
 //import AVFoundation
 
 class StoreScene: SKScene {
-    var menu: SKLabelNode?
-    var inv: SKLabelNode?
+    var menu: SKSpriteNode?
+    var inv: SKSpriteNode?
     var itemSpaces: [ItemSpaceNode] = []
     var items: [ItemClass] = []
     var firstLoad = true
@@ -27,25 +27,40 @@ class StoreScene: SKScene {
         /* Setup your scene here */
        //add menu option node here
         self.backgroundColor = UIColor.grayColor()
-        let background = SKSpriteNode(imageNamed: "Store_1.png")
+        let background = SKSpriteNode(imageNamed: "Store_2.png")
         background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
         background.size = CGSize(width: self.frame.width, height: self.frame.height)
         background.zPosition = -1
         self.addChild(background)
+        //add shelves
+        let shelf1 = SKSpriteNode(imageNamed: "Store_Shelf_1.png")
+        shelf1.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame) - 170)
+        shelf1.setScale(0.4)
+        self.addChild(shelf1)
+        let shelf2 = SKSpriteNode(imageNamed: "Store_Shelf_1.png")
+        shelf2.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame) - 310)
+        shelf2.setScale(0.4)
+        self.addChild(shelf2)
+        let shelf3 = SKSpriteNode(imageNamed: "Store_Shelf_1.png")
+        shelf3.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame) - 450)
+        shelf3.setScale(0.4)
+        self.addChild(shelf3)
+        //shelf1.zPosition = -1
+        //stats label
         statLabel = SKLabelNode.init(text: "")
         statLabel!.position = CGPointMake(self.frame.midX, self.frame.minY + 75)
         statLabel!.name = "stats"
         statLabel!.setScale(0.7)
         self.addChild(statLabel!)
-        menu = SKLabelNode.init(text: "Main Menu")
-        menu!.position = CGPointMake(self.frame.maxX - 70, self.frame.minY + 40)
+        menu = SKSpriteNode(imageNamed: "Zones__Button_1")
+        menu!.position = CGPointMake(self.frame.minX + 50, self.frame.maxY - 50)
         menu!.name = "menu"
-        menu!.setScale(0.7)
+        menu!.setScale(0.3)
         self.addChild(menu!)
-        inv = SKLabelNode.init(text: "Inventory")
-        inv!.position = CGPointMake(self.frame.midX, self.frame.minY + 40)
+        inv = SKSpriteNode(imageNamed: "Fitting_Room__Button_1")
+        inv!.position = CGPointMake(self.frame.maxX - 50, self.frame.maxY - 50)
         inv!.name = "inventory"
-        inv!.setScale(0.7)
+        inv!.setScale(0.3)
         self.addChild(inv!)
         //gold node
         goldCount = (self.userData?.objectForKey("inventory") as Inventory).gold
