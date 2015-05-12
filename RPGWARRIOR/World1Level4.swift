3e08eab8b6fb81bb9c4d4bb01eb5cdb3b691a60d
zach-witt <zwitt@tamu.edu>
2015-05-12 02:02:13 -0500

    3 and 4

--- RPGWARRIOR/World1Level4.swift
+++ RPGWARRIOR/World1Level4.swift
@@ -19,7 +19,12 @@ class World1Level4: SKScene, SKPhysicsContactDelegate {
     var levelOver = false
     let levelName = "world1level4"
     var droppedItem = false
-    
+    //REGEN CODE******
+    var lastHeal: Double = 0.0
+    let healSpeed = 5.0
+    var lifeNode: SKLabelNode?
+    var maxLife: CGFloat = 0.0
+    //*****************
     let wizardAttackSpeed = 1.0
     
     var theBomber: BomberClass?
@@ -29,7 +34,14 @@ class World1Level4: SKScene, SKPhysicsContactDelegate {
         /* Setup your scene here */
         theHero = HeroClass.makeHero(CGPointMake(self.frame.midX, 30))
         theHero!.setScale(0.6)
+        theHero!.name = "hero"
         self.addChild(theHero!)
+        lifeNode = SKLabelNode(text: "\(Int(floor(theHero!.life!)))")
+        lifeNode!.position = CGPointMake(self.frame.maxX - 20, self.frame.maxY - 20)
+        lifeNode!.name = "life"
+        lifeNode!.fontColor = UIColor.redColor()
+        lifeNode!.fontSize = 20
+        self.addChild(lifeNode!)
         theBomber = BomberClass.makeBomber(CGPointMake(self.frame.midX, self.frame.maxY - 50))
         self.addChild(theBomber!)
         //the below constraints did nothing
@@ -37,11 +49,16 @@ class World1Level4: SKScene, SKPhysicsContactDelegate {
         //ourHero.constraints = [distanceConstraint]
         let background = SKSpriteNode(imageNamed: "Beach_Background_1.png")
         background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+        background.name = "background"
         background.size = CGSize(width: self.frame.width, height: self.frame.height)
         background.zPosition = -1
         self.physicsWorld.contactDelegate = self
         self.addChild(background)
         theHero!.updateStats()
+        //*****REGENE CODE****
+        maxLife = theHero!.life!
+        //********************
+        
         self.addChild(MiniCrab.crabAtPosition(CGPointMake(self.frame.maxX - 10, 150), endPosition: CGPointMake(10, 150)))
         self.addChild(MiniCrab.crabAtPosition(CGPointMake(10, 200), endPosition: CGPointMake(self.frame.maxX - 10, 200)))
         self.addChild(MiniCrab.crabAtPosition(CGPointMake(self.frame.maxX - 10, 250), endPosition: CGPointMake(10, 250)))
@@ -105,38 +122,43 @@ class World1Level4: SKScene, SKPhysicsContactDelegate {
         }
         self.totalGameTime += currentTime - self.lastUpdatesTime
         
+        //******REGEN CODE
+        if currentTime - lastHeal  > healSpeed{
+            self.lastHeal = currentTime
+            if theHero!.life < maxLife{
+                theHero!.life! += theHero!.regeneration!
+                if theHero!.life > maxLife{
+                    theHero!.life = maxLife
+                }
+            }
+        }
         self.lastUpdatesTime = currentTime
+        lifeNode!.text = "\(Int(floor(theHero!.life!)))"
+        //***************
         
+        //win condition
         //check for win condition
         if (theBomber!.isDead || theHero!.life <= 0) && !levelOver{
-            //parent of self is viewcontroller, has view, extends sknode
-            //if (theHero!.life == 0){
-            //   let deathNode = SKLabelNode.init(text: "You died, try again!")
-            //   deathNode.position = CGPointMake(self.frame.midX, self.frame.midY)
-            //   self.addChild(deathNode)
-            // }else if (theBomber!.isDead){
-            //   let winNode = SKLabelNode.init(text: "You win, congratulations!")
-            //   winNode.position = CGPointMake(self.frame.midX, self.frame.midY)
-            //   self.addChild(winNode)
-            // }
-            if (self.childNodeWithName("item") == nil && droppedItem) || theHero!.life <= 0{
-                //let menuScene = MainMenuScene(size: self.frame.size)
-                //println("got here111")
-                //(self.userData?.objectForKey("menu") as MainMenuScene).userData?.setObject(self.userData?.objectForKey("inventory") as Inventory, forKey: "inventory")
-                //println("got here222")
-                //menuScene.userData?.setValue(self.userData?.objectForKey("inventory"), forKey: "inventory")
+            
+            if (self.childNodeWithName("gold") == nil && self.childNodeWithName("item") == nil && droppedItem) || theHero!.life <= 0{
+                
                 let skTransition = SKTransition.fadeWithDuration(5.0)
-                //let gameScene = self.userData?.objectForKey("menu") as MainMenuScene
+                
                 self.view?.presentScene(self.userData?.objectForKey("menu") as MainMenuScene, transition: skTransition)
+                
                 levelOver = true
             }
-            else if (self.childNodeWithName("item") == nil){
+            else if (self.childNodeWithName("item") == nil && self.childNodeWithName("gold") == nil){
                 if theBomber!.isDead{
-                    dropLoot("world1level4", self, CGPointMake(self.frame.midX, self.frame.midY), CGSizeMake(30, 30))
+                    dropLoot("level4", self, CGPointMake(self.frame.midX, self.frame.midY), CGSizeMake(30, 30))
                     droppedItem = true
+                    for node in self.children{
+                        if node.name != "background" && node.name != "item" && node.name != "hero" && node.name != "wizard" && node.name != "life" && node.name != "gold"{
+                            node.removeFromParent()
+                        }
+                    }
                 }
             }
         }
     }
 }
-
