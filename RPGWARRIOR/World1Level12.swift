9fe35ad478c93950ed18a02212162a017b5bf6ec
zach-witt <zwitt@tamu.edu>
2015-05-12 23:12:16 -0500

    lvl balance

---
+++ RPGWARRIOR/World1Level12.swift
@@ -0,0 +1,173 @@
+//
+//  World1Level15.swift
+//  RPGWARRIOR
+//
+//  Created by Drew Zoellner on 4/2/15.
+//  Copyright (c) 2015 Drew Zoellner. All rights reserved.
+//
+import SpriteKit
+
+//import AVFoundation
+
+class World1Level12: SKScene, SKPhysicsContactDelegate {
+    
+    var gameStartTime = 0.0
+    var totalGameTime = 0.0
+    var lastUpdatesTime = 0.0
+    var lastWave: Double = 0.0
+    var levelOver = false
+    let levelName = "world1level12"
+    var droppedItem = false
+    //REGEN CODE******
+    var lastHeal: Double = 0.0
+    let healSpeed = 5.0
+    var lifeNode: SKLabelNode?
+    var maxLife: CGFloat = 0.0
+    //*****************
+    let whaleAttackSpeed = 3.0
+    var whichWave = 0
+    var wavePositions: [CGPoint]?
+    
+    
+    var theWhale: WhaleBoss?
+    var theHero: HeroClass?
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        theHero = HeroClass.makeHero(CGPointMake(self.frame.midX, 30))
+        theHero!.setScale(0.6)
+        theHero!.name = "hero"
+        self.addChild(theHero!)
+        wavePositions = [
+            CGPointMake(-20, 100),
+            CGPointMake(self.frame.maxX + 20, 200),
+            CGPointMake(-20, 300),
+            CGPointMake(self.frame.maxX + 20, 400),
+            CGPointMake(-20, 300),
+            CGPointMake(self.frame.maxX + 20, 200),
+            CGPointMake(-20, 100),
+            CGPointMake(self.frame.maxX + 20, 200),
+            CGPointMake(-20, 300),
+            CGPointMake(self.frame.maxX + 20, 400),
+            CGPointMake(-20, 500),
+            CGPointMake(self.frame.maxX + 20, 600)]
+        lifeNode = SKLabelNode(text: "\(Int(floor(theHero!.life!)))")
+        lifeNode!.position = CGPointMake(self.frame.maxX - 20, self.frame.maxY - 20)
+        lifeNode!.fontColor = UIColor.redColor()
+        lifeNode!.fontSize = 20
+        self.addChild(lifeNode!)
+        theWhale = WhaleBoss.makeWhale(CGPointMake(self.frame.midX, self.frame.maxY - 50))
+        self.addChild(theWhale!)
+        //the below constraints did nothing
+        //let distanceConstraint = SKConstraint.distance(SKRange(lowerLimit: 10), toNode: aWizard)
+        //ourHero.constraints = [distanceConstraint]
+        let background = SKSpriteNode(imageNamed: "Beach_Background_1.png")
+        background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+        background.name = "background"
+        background.size = CGSize(width: self.frame.width, height: self.frame.height)
+        background.zPosition = -1
+        self.addChild(background)
+        self.physicsWorld.contactDelegate = self
+        theHero!.updateStats()
+        //*****REGENE CODE****
+        maxLife = theHero!.life!
+        //********************
+        
+        
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody!
+        var secondBody: SKPhysicsBody!
+        if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask){
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        }else{
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        //HERO VS WAVE
+        if (firstBody.categoryBitMask == CollisionBitMasks.collisionCategoryHero.rawValue &&
+            secondBody.categoryBitMask == CollisionBitMasks.collisionCategoryWave.rawValue){
+                theHero!.takeDamage(5)
+        }
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        let aHero = self.childNodeWithName("hero") as HeroClass
+        let aWhale = self.childNodeWithName("whale") as WhaleBoss
+        for touch in touches{
+            aHero.moveHelper(touch.locationInNode(self))
+        }
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        if self.gameStartTime == 0 {
+            self.gameStartTime = currentTime
+            self.lastUpdatesTime = currentTime
+            self.lastWave = currentTime
+        }
+        self.totalGameTime += currentTime - self.lastUpdatesTime
+        
+        //******REGEN CODE
+        if currentTime - lastHeal  > healSpeed{
+            self.lastHeal = currentTime
+            if theHero!.life < maxLife{
+                theHero!.life! += theHero!.regeneration!
+            }
+        }
+        
+        if currentTime - lastWave  > whaleAttackSpeed && !levelOver{
+            self.lastWave = currentTime
+            if whichWave == wavePositions!.count {
+                whichWave = 0
+            }
+            theWhale!.throwWave(wavePositions![whichWave])
+            whichWave += 1
+            theWhale!.throwWave(wavePositions![whichWave])
+            whichWave += 1
+        }
+        self.totalGameTime += currentTime - self.lastUpdatesTime
+        
+        //******REGEN CODE
+        if currentTime - lastHeal  > healSpeed{
+            self.lastHeal = currentTime
+            if theHero!.life < maxLife{
+                theHero!.life! += theHero!.regeneration!
+                if theHero!.life > maxLife{
+                    theHero!.life = maxLife
+                }
+            }
+        }
+        self.lastUpdatesTime = currentTime
+        lifeNode!.text = "\(Int(floor(theHero!.life!)))"
+        //***************
+        
+        //win condition
+        //check for win condition
+        if (theWhale!.isDead || theHero!.life <= 0) && !levelOver{
+            
+            if (self.childNodeWithName("gold") == nil && self.childNodeWithName("item") == nil && droppedItem) || theHero!.life <= 0{
+                
+                let skTransition = SKTransition.fadeWithDuration(5.0)
+                
+                self.view?.presentScene(self.userData?.objectForKey("menu") as MainMenuScene, transition: skTransition)
+                
+                levelOver = true
+            }
+            else if (self.childNodeWithName("item") == nil && self.childNodeWithName("gold") == nil){
+                if theWhale!.isDead{
+                    dropLoot("level12", self, CGPointMake(self.frame.midX, self.frame.midY), CGSizeMake(30, 30))
+                    droppedItem = true
+                    for node in self.children{
+                        if node.name != "background" && node.name != "item" && node.name != "hero" && node.name != "whale" && node.name != "life" && node.name != "gold"{
+                            node.removeFromParent()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
