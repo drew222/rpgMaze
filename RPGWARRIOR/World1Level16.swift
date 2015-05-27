d8cc85450058859572dddf1413e10d526d9f972b
Drew Zoellner <andrewzoellner@hotmail.com>
2015-05-27 17:56:52 -0500

    thru 17

--- RPGWARRIOR/World1Level16.swift
+++ RPGWARRIOR/World1Level16.swift
@@ -23,12 +23,18 @@ class World1Level16: SKScene, SKPhysicsContactDelegate {
     //REGEN CODE******
     var lastHeal: Double = 0.0
     let healSpeed = 5.0
-    var lifeNode: SKLabelNode?
     var maxLife: CGFloat = 0.0
     //*****************
     let whaleAttackSpeed = 2.0
     var whichWave = 0
     var wavePositions: [CGPoint]?
+    
+    //Ink / Life / Chest Changes*****
+    var inkSplatted = false
+    var newLifeNode: SKSpriteNode?
+    var clickedChest = false
+    var droppedChest = false
+    //*******************************
     
     
     var theWhale: WhaleBoss?
@@ -49,11 +55,6 @@ class World1Level16: SKScene, SKPhysicsContactDelegate {
             CGPointMake(self.frame.maxX + 20, 500),
             CGPointMake(-20, 400),
             CGPointMake(self.frame.maxX + 20, 500)]
-        lifeNode = SKLabelNode(text: "\(Int(floor(theHero!.life!)))")
-        lifeNode!.position = CGPointMake(self.frame.maxX - 20, self.frame.maxY - 20)
-        lifeNode!.fontColor = UIColor.redColor()
-        lifeNode!.fontSize = 20
-        self.addChild(lifeNode!)
         theWhale = WhaleBoss.makeWhale(CGPointMake(self.frame.midX, self.frame.maxY - 50))
         self.addChild(theWhale!)
         //the below constraints did nothing
@@ -70,6 +71,21 @@ class World1Level16: SKScene, SKPhysicsContactDelegate {
         //*****REGENE CODE****
         maxLife = theHero!.life!
         //********************
+        
+        //Ink / Life / Chest Changes*****
+        newLifeNode = SKSpriteNode(imageNamed: "World_1_Level_\(Int(theHero!.life!))_Text")
+        newLifeNode!.position = CGPointMake(self.frame.maxX - 20, self.frame.maxY - 20)
+        newLifeNode!.size = CGSizeMake(10, 10)
+        newLifeNode!.name = "lifeNumber"
+        newLifeNode!.zPosition = 3
+        self.addChild(newLifeNode!)
+        
+        let lifeHeart = SKSpriteNode(imageNamed: "Life_Symbol_1")
+        lifeHeart.position = CGPointMake(self.frame.maxX - 20, self.frame.maxY - 20)
+        lifeHeart.name = "lifeheart"
+        lifeHeart.setScale(0.15)
+        self.addChild(lifeHeart)
+        //************************************
         
         //middle
         for (var i = 20; i < Int(self.frame.maxX) - 20; i += 40){
@@ -132,10 +148,26 @@ class World1Level16: SKScene, SKPhysicsContactDelegate {
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        let aHero = self.childNodeWithName("hero") as! HeroClass
-        let aWhale = self.childNodeWithName("whale") as! WhaleBoss
+        let aHero = self.childNodeWithName("hero") as? HeroClass
         for touch in touches{
-            aHero.moveHelper((touch as! UITouch).locationInNode(self))
+            if !inkSplatted{
+                aHero!.moveHelper((touch as! UITouch).locationInNode(self))
+            }else{
+                if self.childNodeWithName("yesText")!.containsPoint((touch as! UITouch).locationInNode(self)){
+                    let newLevel1 = World1Level16(size: self.frame.size)
+                    newLevel1.userData = NSMutableDictionary()
+                    newLevel1.userData?.setObject(self.userData?.objectForKey("inventory") as! Inventory, forKey: "inventory")
+                    newLevel1.userData?.setObject(self.userData?.objectForKey("menu") as! MainMenuScene, forKey: "menu")
+                    //level2.userData? = ["menu" : self, "inventory" : self.userData?.objectForKey("inventory") as Inventory]
+                    let skTransition = SKTransition.fadeWithDuration(1.0)
+                    self.view?.presentScene(newLevel1, transition: skTransition)
+                    
+                }else if self.childNodeWithName("noText")!.containsPoint((touch as! UITouch).locationInNode(self)){
+                    let skTransition = SKTransition.fadeWithDuration(1.0)
+                    
+                    self.view?.presentScene(self.userData?.objectForKey("menu") as! MainMenuScene, transition: skTransition)
+                }
+            }
         }
     }
     
@@ -159,39 +191,58 @@ class World1Level16: SKScene, SKPhysicsContactDelegate {
             }
         }
         self.lastUpdatesTime = currentTime
-        lifeNode!.text = "\(Int(floor(theHero!.life!)))"
-        //***************
-        
-        if currentTime - lastWave  > whaleAttackSpeed && !levelOver{
-            self.lastWave = currentTime
-            if whichWave == wavePositions!.count {
-                whichWave = 0
-            }
-            theWhale!.throwWave(wavePositions![whichWave])
-            whichWave += 1
-        }
-        
-        //win condition
-        //check for win condition
+        newLifeNode!.texture = SKTexture(imageNamed: "World_1_Level_\(Int(theHero!.life!))_Text")
         if (theWhale!.isDead || theHero!.life <= 0) && !levelOver{
             
             if (self.childNodeWithName("gold") == nil && self.childNodeWithName("item") == nil && droppedItem) || theHero!.life <= 0{
                 
-                let skTransition = SKTransition.fadeWithDuration(1.0)
-                
-                self.view?.presentScene(self.userData?.objectForKey("menu") as! MainMenuScene, transition: skTransition)
+                //INK SPLAT CODE
+                if theHero!.life <= 0 {
+                    let inkSplat = SKSpriteNode(imageNamed: "Ink_Splat_1")
+                    for node in self.children{
+                        if (node as? SKSpriteNode != nil) && node.name != "background"{
+                            node.removeFromParent()
+                        }
+                    }
+                    inkSplat.position = CGPointMake(self.frame.midX, self.frame.midY)
+                    inkSplat.size = CGSizeMake(50, 50)
+                    self.addChild(inkSplat)
+                    let stretchAction = SKAction.scaleXBy(7, y: 7, duration: 0.4)
+                    let codeBlock = SKAction.runBlock({
+                        let yesText = SKSpriteNode(imageNamed: "Yes_Text_1")
+                        let noText = SKSpriteNode(imageNamed: "No_Text_1")
+                        yesText.size = CGSizeMake(75, 40)
+                        noText.size = CGSizeMake(75, 40)
+                        yesText.position = CGPointMake(self.frame.midX - 60, self.frame.midY - 30)
+                        noText.position = CGPointMake(self.frame.midX + 60, self.frame.midY - 30)
+                        yesText.name = "yesText"
+                        noText.name = "noText"
+                        self.addChild(yesText)
+                        self.addChild(noText)
+                    })
+                    let sequence = SKAction.sequence([stretchAction, codeBlock])
+                    inkSplat.runAction(sequence)
+                    inkSplatted = true
+                }else{
+                    let skTransition = SKTransition.fadeWithDuration(1.0)
+                    
+                    self.view?.presentScene(self.userData?.objectForKey("menu") as! MainMenuScene, transition: skTransition)
+                }
                 
                 levelOver = true
             }
             else if (self.childNodeWithName("item") == nil && self.childNodeWithName("gold") == nil){
-                if theWhale!.isDead{
+                if theWhale!.isDead && droppedChest && (self.childNodeWithName("chest") as! TreasureChest).open{
                     dropLoot("level16", self, CGPointMake(self.frame.midX, self.frame.midY), CGSizeMake(30, 30))
                     droppedItem = true
+                }else if theWhale!.isDead && !droppedChest {
                     for node in self.children{
-                        if node.name != "background" && node.name != "item" && node.name != "hero" && node.name != "whale" && node.name != "life" && node.name != "gold"{
+                        if (node as? SKSpriteNode != nil) && node.name != "background" && node.name != "item" && node.name != "hero" && node.name != "whale" && node.name != "life" && node.name != "gold" && node.name != "chest"{
                             node.removeFromParent()
                         }
                     }
+                    self.addChild(TreasureChest.chestAtPosition(CGPointMake(self.frame.midX, self.frame.midY)))
+                    droppedChest = true
                 }
             }
         }
